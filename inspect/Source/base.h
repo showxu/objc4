44f2fa04d32944813952ee2eb1039a3b976c5a5c
showxdxu <showxdxu@tencent.com>
2020-04-08 18:22:34 +0800

    feat(objc-inspect): update objc-inspect target for latest objc4 (objc-781)

--- inspect/Source/base.h
+++ inspect/Source/base.h
@@ -2,7 +2,7 @@
 //  base.h
 //  Magicians
 //
-//  Created by Alchemist on 2016/12/2.
+//  Created by 0xxd0 on 2016/12/2.
 //  Copyright © 2016年 alchemistxxd. All rights reserved.
 //
 
@@ -52,4 +52,3 @@ _Pragma("clang diagnostic pop")
 #if defined(__cplusplus)
 }
 #endif
-
