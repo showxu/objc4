53d7ddb4cdf8ad000a95ef15dc68c054cc37948c
showxu <show.xdxu@icloud.com>
2020-11-22 00:27:21 +0800

    feat(objc4): Update for objc4 787.1

--- objc4/runtime/Protocol.h
+++ objc4/runtime/Protocol.h
@@ -76,7 +76,7 @@ OBJC_AVAILABLE(10.0, 2.0, 9.0, 1.0, 2.0)
     __TVOS_DEPRECATED(9.0, 9.0, "use protocol_getMethodDescription instead") 
     __WATCHOS_DEPRECATED(1.0, 1.0, "use protocol_getMethodDescription instead")
 #ifndef __APPLE_BLEACH_SDK__
-    __BRIDGEOS_DEPRECATED(2.0, 2.0, "use protocol_getMethodDescription instead")
+//    __BRIDGEOS_DEPRECATED(2.0, 2.0, "use protocol_getMethodDescription instead")
 #endif
 ;
 - (struct objc_method_description *) descriptionForClassMethod:(SEL)aSel 
@@ -85,7 +85,7 @@ OBJC_AVAILABLE(10.0, 2.0, 9.0, 1.0, 2.0)
     __TVOS_DEPRECATED(9.0, 9.0, "use protocol_getMethodDescription instead") 
     __WATCHOS_DEPRECATED(1.0, 1.0, "use protocol_getMethodDescription instead")
 #ifndef __APPLE_BLEACH_SDK__
-    __BRIDGEOS_DEPRECATED(2.0, 2.0, "use protocol_getMethodDescription instead")
+//    __BRIDGEOS_DEPRECATED(2.0, 2.0, "use protocol_getMethodDescription instead")
 #endif
 ;
 
